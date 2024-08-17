176e86a22ae6f68edf39e2b25207090193d17f5a
Joalor64 <rhgarcon@gmail.com>
2024-08-17 13:00:09 -0400

    Update SongSelectState.hx

--- source/states/SongSelectState.hx
+++ source/states/SongSelectState.hx
@@ -113,7 +113,7 @@ class SongSelectState extends ExtendableState {
 		}
 	}
 
-	public function boundTo(value:Float, min:Float, max:Float):Float {
+	public static function boundTo(value:Float, min:Float, max:Float):Float {
 		return Math.max(min, Math.min(max, value));
 	}
 
