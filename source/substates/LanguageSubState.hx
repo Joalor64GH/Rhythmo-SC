c6afb239cb6ffa467db2ec554c00c8ad2f06ec06
Joalor64 <rhgarcon@gmail.com>
2024-09-13 13:48:43 -0400

    Update LanguageSubState.hx

--- source/substates/LanguageSubState.hx
+++ source/substates/LanguageSubState.hx
@@ -25,11 +25,14 @@ class LanguageSubState extends ExtendableSubState {
 		}
 
 		var bg:FlxSprite = new FlxSprite().makeGraphic(1280, 720, FlxColor.BLACK);
+		bg.scrollFactor.set();
+		bg.screenCenter();
 		bg.alpha = 0.6;
 		add(bg);
 
 		var title:FlxText = new FlxText(0, 0, 0, Localization.get("langSelect", SaveData.settings.lang), 12);
 		title.setFormat(Paths.font('vcr.ttf'), 70, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		text.scrollFactor.set();
 		title.screenCenter(X);
 		add(title);
 
@@ -39,6 +42,7 @@ class LanguageSubState extends ExtendableSubState {
 		for (i in 0...langStrings.length) {
 			var text:FlxText = new FlxText(0, 300 + (i * 70), 0, langStrings[i].lang, 32);
 			text.setFormat(Paths.font('vcr.ttf'), 80, FlxColor.WHITE, FlxTextAlign.CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+			text.scrollFactor.set();
 			text.screenCenter(X);
 			text.ID = i;
 			group.add(text);
@@ -46,6 +50,7 @@ class LanguageSubState extends ExtendableSubState {
 
 		var noticeTxt:FlxText = new FlxText(5, FlxG.height - 24, 0, Localization.get("langNotCompletelyAccurate", SaveData.settings.lang), 12);
 		noticeTxt.setFormat(Paths.font('vcr.ttf'), 26, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		noticeTxt.scrollFactor.set();
 		noticeTxt.screenCenter(X);
 		add(noticeTxt);
 
