0e51907d5038ed6e986300d544f832a22a70a4ad
Joalor64 <rhgarcon@gmail.com>
2024-09-21 22:04:45 -0400

    fps border thingy

--- source/backend/FPS.hx
+++ source/backend/FPS.hx
@@ -1,10 +1,26 @@
 package backend;
 
-class FPS extends openfl.text.TextField {
+import openfl.text.*;
+
+class FPS extends TextField {
 	var times:Array<Float> = [];
+
+	public var borderSize:Int = 1;
+	@:noCompletion private final borders:Array<TextField> = new Array<TextField>();
 
 	public function new(x:Float, y:Float, color:Int, ?font:String) {
 		super();
+
+		var border:TextField;
+		for (i in 0...8) {
+			borders.push(border = new TextField());
+			border.selectable = false;
+			border.mouseEnabled = false;
+			border.autoSize = LEFT;
+			border.multiline = true;
+			border.width = 1280;
+			border.height = 720;
+		}
 
 		text = "";
 		this.x = x;
@@ -12,7 +28,7 @@ class FPS extends openfl.text.TextField {
 		width = 1280;
 		height = 720;
 		selectable = false;
-		defaultTextFormat = new openfl.text.TextFormat(Paths.font((font != null) ? font : 'vcr.ttf'), 12, color);
+		defaultTextFormat = new TextFormat(Paths.font((font != null) ? font : 'vcr.ttf'), 16, color);
 		alpha = 0.8; // to see things from the back better
 
 		addEventListener(openfl.events.Event.ENTER_FRAME, (_) -> {
@@ -30,5 +46,41 @@ class FPS extends openfl.text.TextField {
 
 			textColor = (times.length < FlxG.drawFramerate * 0.5) ? 0xFFFF0000 : 0xFFFFFFFF;
 		});
+
+		addEventListener(Event.REMOVED, function(_) {
+			for (border in borders) this.parent.removeChild(border);
+		});
+
+		addEventListener(Event.ADDED, function(_) {
+			for (border in borders) this.parent.addChildAt(border, this.parent.getChildIndex(this));
+		});
+
+		@:noCompletion override function set_visible(value:Bool):Bool {
+			for (border in borders) border.visible = value;
+			return super.set_visible(value);
+		}
+
+		@:noCompletion override function set_defaultTextFormat(value:TextFormat):TextFormat {
+			for (border in borders) {
+				border.defaultTextFormat = value;
+				border.textColor = 0xFF000000;
+			}
+			return super.set_defaultTextFormat(value);
+		}
+
+		@:noCompletion override function set_x(x:Float):Float {
+			for (i in 0...8) borders[i].x = x + ([0, 3, 5].contains(i) ? borderSize : [2, 4, 7].contains(i) ? -borderSize : 0);
+			return super.set_x(x);
+		}
+
+		@:noCompletion override function set_y(y:Float):Float {
+			for (i in 0...8) borders[i].y = y + ([0, 1, 2].contains(i) ? borderSize : [5, 6, 7].contains(i) ? -borderSize : 0);
+			return super.set_y(y);
+		}
+
+		@:noCompletion override function set_text(text:String):String {
+			for (border in borders) border.text = text;
+			return super.set_text(text);
+		}
 	}
 }
