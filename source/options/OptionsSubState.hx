d01b5ee140804ddba45caef8c63eca51dc84a4d2
Joalor64 <rhgarcon@gmail.com>
2024-10-16 21:09:36 -0400

    Update OptionsSubState.hx

--- source/options/OptionsSubState.hx
+++ source/options/OptionsSubState.hx
@@ -3,7 +3,7 @@ package options;
 import options.Option;
 
 class OptionsSubState extends ExtendableSubState {
-	var options:Array<Option>;
+	var options:Array<Option> = [];
 	var grpOptions:FlxTypedGroup<FlxText>;
 	var curSelected:Int = 0;
 	var description:FlxText;
@@ -137,6 +137,5 @@ class OptionsSubState extends ExtendableSubState {
 	public function addOption(option:Option) {
 		if (options == null || options.length < 1) options = [];
 		options.push(option);
-		return option;
 	}
 }
