04c706c1e398e34e9fecec570a0a5ced0b0dbf44
Joalor64 <rhgarcon@gmail.com>
2024-10-20 09:08:27 -0400

    revert idk

--- source/objects/AudioDisplay.hx
+++ source/objects/AudioDisplay.hx
@@ -8,68 +8,52 @@ class AudioDisplay extends FlxSpriteGroup {
 	public var snd:FlxSound;
 
 	var _height:Int;
+	var gap:Float = 2;
 	var line:Int;
 
-	public function new(snd:FlxSound = null, X:Float = 0, Y:Float = 0, Width:Int, Height:Int, line:Int, gap:Int, Color:FlxColor) {
-		super(X, Y);
+	public function new(snd:FlxSound, x:Float = 0, y:Float = 0, Width:Int, Height:Int, line:Int, Color:FlxColor) {
+		super(x, y);
 
 		this.snd = snd;
 		this.line = line;
 
 		for (i in 0...line) {
 			var newLine = new FlxSprite().makeGraphic(Std.int(Width / line - gap), 1, Color);
-			newLine.x = (Width / line) * i;
+			newLine.x = (Std.int(Width / line) + gap) * i;
 			add(newLine);
 		}
 		_height = Height;
 		@:privateAccess
-		if (snd != null) {
-			analyzer = new SpectralAnalyzer(snd._channel.__audioSource, Std.int(line * 1 + Math.abs(0.05 * 4)), 1, 5);
+		if (snd._channel != null) {
+			analyzer = new SpectralAnalyzer(snd._channel.__audioSource, line, 1, 5);
 			analyzer.fftN = 256;
 		}
 	}
 
-	public var stopUpdate:Bool = false;
-
 	override function update(elapsed:Float) {
-		if (stopUpdate)
+		addAnalyzer();
+		if (analyzer == null)
 			return;
-
 		var levels = analyzer.getLevels();
 
 		for (i in 0...members.length) {
 			var animFrame:Int = Math.round(levels[i].value * _height);
 
+			#if desktop
 			animFrame = Math.round(animFrame * FlxG.sound.volume);
+			#end
 
 			members[i].scale.y = FlxMath.lerp(animFrame, members[i].scale.y, Math.exp(-elapsed * 16));
-			if (members[i].scale.y < _height / 40)
-				members[i].scale.y = _height / 40;
 			members[i].y = this.y - members[i].scale.y / 2;
 		}
-
 		super.update(elapsed);
 	}
 
-	function addAnalyzer(snd:FlxSound) {
+	function addAnalyzer() {
 		@:privateAccess
-		if (snd != null && analyzer == null) {
-			analyzer = new SpectralAnalyzer(snd._channel.__audioSource, Std.int(line * 1 + Math.abs(0.05 * 4)), 1, 5);
+		if (snd._channel != null && analyzer == null) {
+			analyzer = new SpectralAnalyzer(snd._channel.__audioSource, line, 1, 5);
 			analyzer.fftN = 256;
-		}
-	}
-
-	public function changeAnalyzer(snd:FlxSound) {
-		@:privateAccess
-		analyzer.changeSnd(snd._channel.__audioSource);
-
-		stopUpdate = false;
-	}
-
-	public function clearUpdate() {
-		for (i in 0...members.length) {
-			members[i].scale.y = _height / 40;
-			members[i].y = this.y - members[i].scale.y / 2;
 		}
 	}
 }
