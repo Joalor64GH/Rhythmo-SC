5cf55db42892f49903699a48a811ec14fd5c512d
Joalor64 <rhgarcon@gmail.com>
2024-12-06 10:27:38 -0500

    Update Achievements.hx

--- source/backend/Achievements.hx
+++ source/backend/Achievements.hx
@@ -115,7 +115,7 @@ class Achievements {
 		achBG.scrollFactor.set();
 		sprGroup.add(achBG);
 
-		var achIcon:FlxSprite = new FlxSprite(achBG.x + 10, achBG.y + 10).loadGraphic(Paths.image('achievements/$ach'));
+		var achIcon:FlxSprite = new GameSprite(achBG.x + 10, achBG.y + 10).loadGraphic(Paths.image('achievements/$ach'));
 		achIcon.setGraphicSize(Std.int(achIcon.width * (2 / 3)));
 		achIcon.scrollFactor.set();
 		achIcon.updateHitbox();
